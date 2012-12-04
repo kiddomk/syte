481a74e145048337fdb83fe78b1e709bd57f83f2
Jun S <kiddo.mk@gmail.com>
2012-12-04 16:08:35 +0000

    upload to heroku

--- syte/manage.py
+++ syte/manage.py
@@ -7,8 +7,4 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-<<<<<<< HEAD
     execute_from_command_line(sys.argv)
-=======
-    execute_from_command_line(sys.argv)
->>>>>>> d5c56b2883b67368d81f08c03978cb441b74cb43
